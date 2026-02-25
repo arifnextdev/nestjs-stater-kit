d8420ca2fe84546b36709b74f9875acd282486c9
Md Ariful Islam <arif171042@gmail.com>
2026-02-25 16:37:53 +0600

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { config } from 'dotenv';
+import { PrismaClient } from '../src/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import * as bcrypt from 'bcrypt';
 
-const prisma = new PrismaClient();
+config();
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL as string,
+});
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   console.log('🌱 Starting database seeding...');
